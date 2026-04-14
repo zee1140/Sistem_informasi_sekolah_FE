fa62d6e22536407d407d045327e6f41296a7657a
adiyudiana <adiyudiana04@gmail.com>
2026-04-14 07:44:40 +0700

    commit adi first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import router from './router/index.js';
+</script>
+
+<style>
+/* Reset & base style biar konsisten */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Inter', sans-serif;
+}
+</style>
